b214b870a1acabe7253d4963115219434ee7510d
Tusher <tusher16@pop-os.localdomain>
2021-09-21 12:57:16 +0600

    added footer and header

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    
+    return(
+        <div>
+            <footer>
+                Foooter
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    
+    return (
+        <div>
+            <header>
+                Header
+            </header>
+        </div>
+    )
+}
+
+export default Header;
